690810ef669d9b5b54b499562c62bcdfa14c369f
Victor Valdez <52045663+strikervc@users.noreply.github.com>
2021-09-09 18:06:39 -0400

    CRUD uploaded

---
+++ WebCRUD/Controllers/RolController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebCRUD;
+
+namespace WebCRUD.Controllers
+{
+    public class RolController : Controller
+    {
+        private dbArqCRUDEntities db = new dbArqCRUDEntities();
+
+        // GET: Rol
+        public ActionResult Index()
+        {
+            return View(db.Role.ToList());
+        }
+
+        // GET: Rol/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.Role.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // GET: Rol/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rol/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Seq,Name,Description,Enabled,CreatedDate")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                role.Id = Guid.NewGuid();
+                db.Role.Add(role);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(role);
+        }
+
+        // GET: Rol/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.Role.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Rol/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Seq,Name,Description,Enabled,CreatedDate")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(role);
+        }
+
+        // GET: Rol/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.Role.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        // POST: Rol/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Role role = db.Role.Find(id);
+            db.Role.Remove(role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebCRUD/Controllers/UsersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebCRUD;
+
+namespace WebCRUD.Controllers
+{
+    public class UsersController : Controller
+    {
+        private dbArqCRUDEntities db = new dbArqCRUDEntities();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var usuario = db.Usuario.Include(u => u.Role);
+            return View(usuario.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            ViewBag.RoleId = new SelectList(db.Role, "Id", "Name");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Seq,Username,Password,Email,EmailConfirmed,EmailConfirmedDate,Enabled,DisplayName,LastAccessDate,PictureUrl,RoleId,CreatedDate")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuario.Id = Guid.NewGuid();
+                db.Usuario.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RoleId = new SelectList(db.Role, "Id", "Name", usuario.RoleId);
+            return View(usuario);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RoleId = new SelectList(db.Role, "Id", "Name", usuario.RoleId);
+            return View(usuario);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Seq,Username,Password,Email,EmailConfirmed,EmailConfirmedDate,Enabled,DisplayName,LastAccessDate,PictureUrl,RoleId,CreatedDate")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RoleId = new SelectList(db.Role, "Id", "Name", usuario.RoleId);
+            return View(usuario);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            db.Usuario.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
